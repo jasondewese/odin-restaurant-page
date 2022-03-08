e4eca5835bb5a0ff426353171512f037b842d436
Jason Dewese <19419757+jasondewese@users.noreply.github.com>
2022-03-07 21:18:18 -0600

    Add reset.css, add fonts, fix pageInit.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import {testFunc} from './pageInit.js';
 import './reset.css';
 import './style.css';
-import {testFunc} from './pageInit.js';
+
 
 
 //document.getElementById('content').appendChild(pageInit);
 
-console.log(testFunc());
+testFunc();
 
-console.log('testing 1, 2, 3...');
