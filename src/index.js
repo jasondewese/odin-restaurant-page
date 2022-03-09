8cd694377b2d3241b2667883405a03e56b59a10a
Jason Dewese <19419757+jasondewese@users.noreply.github.com>
2022-03-09 13:10:28 -0600

    Add structure and imanges to menu

--- src/index.js
+++ src/index.js
@@ -12,14 +12,3 @@ homeInit();
 //Bea-u-tea-ful
 
 //Communi-tea
-/*
-Menu
-    Drinks
-        Uni-tea - boba flight. Strawberry tea, peach tea, passionfruit tea, blueberry tea
-        Swee-tea - You're my swee-tea pie!
-
-    Dessert
-        Strawberry and cream croffle - It's a waffle. It's a croissant. It's your dream come true!
-    
-
-*/
--- src/menu.js
+++ src/menu.js
@@ -2,9 +2,9 @@ import {homeInit} from './homeInit.js';
 import {contactInit} from './contact.js';
 import {createNav} from './createNav.js';
 
-import Croffle from './Croffle.jpg';
-import Sweetea from './Swee-tea.jpg';
-import Unitea from './Uni-tea.jpg';
+import croffleImage from './Croffle.jpg';
+import sweeTeaImage from './Swee-tea.jpg';
+import uniTeaImage from './Uni-tea.jpg';
 
 const menuInit = () => {
     const content = document.getElementById('content');
@@ -16,16 +16,92 @@ const menuInit = () => {
 
         const nav = createNav();
 
-        const menuItem = document.createElement('div');
-        menuItem.classList.add('menuItem');
-        menuItem.textContent = 'This will be the awesome menu';
+        const menuWrapper = document.createElement('div');
+        menuWrapper.classList.add('menuWrapper');
 
-        content.appendChild(nav);
-        content.appendChild(menuItem);
+            const drinksWrapper = document.createElement('div');
+            drinksWrapper.classList.add('drinksWrapper');
 
+                const drinksLabel = document.createElement('div');
+                drinksLabel.classList.add('drinksLabel');
+                drinksLabel.textContent = 'Drinks';
+
+                const uniTea = document.createElement('div');
+                uniTea.classList.add('drink', 'uniTea');
+                uniTea.textContent = 'uni-tea';
+
+                    const uniTeaIcon = new Image();
+                    uniTeaIcon.src = uniTeaImage;
+
+                    const uniTeaDescr = document.createElement('p');
+                    uniTeaDescr.textContent = 'A boba flight. Strawberry, peach, passionfruit, and blueberry tea.';
+
+                    uniTea.appendChild(uniTeaIcon);
+                    uniTea.appendChild(uniTeaDescr);
+
+                const sweeTea = document.createElement('div');
+                sweeTea.classList.add('drink', 'sweeTea');
+                sweeTea.textContent = 'swee-tea';
+
+                    const sweeTeaIcon = new Image();
+                    sweeTeaIcon.src = sweeTeaImage;
+
+                    const sweeTeaDescr = document.createElement('p');
+                    sweeTeaDescr.textContent = "You're my swee-tea pie!";
+
+                    sweeTea.appendChild(sweeTeaIcon);
+                    sweeTea.appendChild(sweeTeaDescr);
+
+            drinksWrapper.appendChild(drinksLabel);
+            drinksWrapper.appendChild(uniTea);
+            drinksWrapper.appendChild(sweeTea);
+
+            const dessertWrapper = document.createElement('div');
+            dessertWrapper.classList.add('dessertWrapper');
+
+                const dessertLabel = document.createElement('div');
+                dessertLabel.textContent = 'Dessert';
+                dessertLabel.classList.add('dessertList');
+
+                const croffle = document.createElement('div');
+                croffle.textContent = 'croffle';
+                croffle.classList.add('dessert', 'croffle');
+
+                    const croffleIcon = new Image();
+                    croffleIcon.src = croffleImage;
+
+                    const croffleDescr = document.createElement('p');
+                    croffleDescr.textContent = "Strawberry and cream croffle - It's a waffle. It's a croissant. It's your dream come true!";
+
+                    croffle.appendChild(croffleIcon);
+                    croffle.appendChild(croffleDescr);
+
+            dessertWrapper.appendChild(dessertLabel);
+            dessertWrapper.appendChild(croffle);
+
+        menuWrapper.appendChild(drinksWrapper);
+        menuWrapper.appendChild(dessertWrapper);
+
+    content.appendChild(nav);
+    content.appendChild(menuWrapper);
+
+        /*
         const croffleImage = new Image();
         croffleImage.src = Croffle;
         menuItem.appendChild(croffleImage);
+        */
+
+        /*
+        Menu
+            Drinks
+                Uni-tea - boba flight. Strawberry tea, peach tea, passionfruit tea, blueberry tea
+                Swee-tea - You're my swee-tea pie!
+
+            Dessert
+                Strawberry and cream croffle - It's a waffle. It's a croissant. It's your dream come true!
+            
+
+        */
 
     document.querySelector('.homeLink').addEventListener('click', function() {
         homeInit();
