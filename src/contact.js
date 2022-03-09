40407a78de85dd5a39cb460f66ab58ad1a37b439
Jason Dewese <19419757+jasondewese@users.noreply.github.com>
2022-03-09 14:27:18 -0600

    Add content and style to contact page

--- src/contact.js
+++ src/contact.js
@@ -12,12 +12,51 @@ const contactInit = () => {
 
         const nav = createNav();
 
-        const contactItem = document.createElement('div');
-        contactItem.classList.add('menuItem');
-        contactItem.textContent = 'This will be the useful contact info';
+        const contactLabel = document.createElement('h1');
+        contactLabel.textContent = 'Contact Us';
+        contactLabel.classList.add('contactLabel');
+
+        const contactWrapper = document.createElement('div');
+        contactWrapper.classList.add('contactWrapper');
+        
+
+            const contactInfo = document.createElement('div');
+            contactInfo.classList.add('contactInfo');
+            
+                const contactHeading = document.createElement('p');
+                contactHeading.textContent = "Contact Information:";
+                contactHeading.classList.add('contactHeading');
+
+                const phone = document.createElement('p');
+                phone.textContent = 'Phone: 123-456-7890';
+
+                const email = document.createElement('p');
+                email.textContent = "Email: communi-tea@email.address";
+
+                const address = document.createElement('p');
+                address.textContent = 'Address: 123 Communitea Lane, Dallas, TX';
+
+            contactInfo.appendChild(contactHeading);
+            contactInfo.appendChild(phone);
+            contactInfo.appendChild(email);
+            contactInfo.appendChild(address);
+
+            contactWrapper.appendChild(contactInfo);
+        
+
+        const footer = document.createElement('footer');
+        footer.textContent = 'This assignment is part of ';
+        footer.classList.add('footer');
+        const odinLink = document.createElement('a');
+        odinLink.textContent = 'The Odin Project';
+        odinLink.setAttribute('href', 'https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/restaurant-page');
+        footer.appendChild(odinLink);
+
 
         content.appendChild(nav);
-        content.appendChild(contactItem);
+        content.appendChild(contactLabel);
+        content.appendChild(contactWrapper);
+        content.appendChild(footer);
 
     document.querySelector('.homeLink').addEventListener('click', function() {
         homeInit();
